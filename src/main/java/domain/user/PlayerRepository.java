b29a947a18fd0ff39b1fd9ad3d190768803fa901
iamsseaa <homingsea@gmail.com>
2021-12-17 15:51:45 +0900

    feat: 플레이어 저장소에 플레이어 추가하는 함수

--- src/main/java/domain/user/PlayerRepository.java
+++ src/main/java/domain/user/PlayerRepository.java
@@ -6,5 +6,7 @@ import java.util.List;
 public class PlayerRepository {
     List<Player> players = new ArrayList<>();
 
-    
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
 }
