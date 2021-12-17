4510d514fe1ef982d6cc097cb2e31f1717d10ffe
iamsseaa <homingsea@gmail.com>
2021-12-17 18:26:53 +0900

    feat: GameManager에서 이름과 베팅 금액을 입력받아 플레이어 객체 생성

--- src/main/java/domain/GameManager.java
+++ src/main/java/domain/GameManager.java
@@ -1,4 +1,25 @@
 package domain;
 
+import domain.divider.NameDivider;
+import domain.user.Player;
+import domain.user.PlayerRepository;
+import view.InputView;
+
+import java.util.List;
+
 public class GameManager {
+    public static void play() {
+
+    }
+
+    public static void enrollPlayers() {
+        String names = InputView.inputName();
+        List<String> nameList = NameDivider.divideNames(names);
+
+        for (String name : nameList) {
+            int bettingMoney = InputView.inputBettingMoney(name);
+            Player player = new Player(name, bettingMoney);
+            PlayerRepository.addPlayer(player);
+        }
+    }
 }
