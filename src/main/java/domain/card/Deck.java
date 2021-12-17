9a6296333160c485d653d36386343659982a2a30
iamsseaa <homingsea@gmail.com>
2021-12-17 18:09:40 +0900

    feat: 전체 카드 랜덤 정렬 함수

--- src/main/java/domain/card/Deck.java
+++ src/main/java/domain/card/Deck.java
@@ -1,4 +1,14 @@
 package domain.card;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+    static List<Card> cards = new ArrayList<>();
+
+    public static void create() {
+        cards.addAll(CardFactory.create());
+        Collections.shuffle(cards);
+    }
 }
