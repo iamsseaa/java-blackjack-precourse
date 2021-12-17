25b4a72979c176ac8ff92d91271f232eae1433c1
iamsseaa <homingsea@gmail.com>
2021-12-17 16:04:12 +0900

    feat: 프로그램 안내 문구 상수 클래스

---
+++ src/main/java/view/Constant.java
@@ -0,0 +1,18 @@
+package view;
+
+public class Constant {
+    public static final String INPUT_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    public static final String INPUT_BETTING_MONEY = "의 베팅 금액은?";
+    public static final String INPUT_MORE_CARD = "는 한장의 카드를 더 받곘습니까?(예는 y, 아니오는 n)";
+
+    public static final String DEALER = "딜러";
+    public static final String AND = "와";
+    public static final String DELIMITER = ",";
+    public static final String COLON = ": ";
+    public static final String CARD = "카드";
+
+    public static final String OUTPUT_DIVIDED_CARD = "에게 2장의 카드를 나누었습니다.";
+    public static final String OUTPUT_DEALER_UNDER_SIXTEEN = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    public static final String DEALER_CARD = DEALER + CARD + COLON;
+    public static final String PLAYER_CARD = CARD + COLON;
+}
