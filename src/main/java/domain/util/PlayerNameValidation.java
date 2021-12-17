625a9c5a2f01d85c379c5b0910a803fac21e0e90
iamsseaa <homingsea@gmail.com>
2021-12-17 17:03:10 +0900

    feat: 이름 입력 중복되는지 확인

--- src/main/java/domain/util/PlayerNameValidation.java
+++ src/main/java/domain/util/PlayerNameValidation.java
@@ -1,5 +1,9 @@
 package domain.util;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class PlayerNameValidation {
     public static void isEmpty(String names) {
         if (names.length() == 0) {
@@ -10,6 +14,14 @@ public class PlayerNameValidation {
     public static void hasEmpty(String names) {
         if (names.contains(ValidationConstant.SPACE)) {
             throw new IllegalArgumentException(ValidationConstant.ERROR_HAS_EMPTY);
+        }
+    }
+
+    public static void isDuplicated(List<String> nameList) {
+        Set<String> nameSet = new HashSet<>(nameList);
+
+        if (nameList.size() != nameSet.size()) {
+            throw new IllegalArgumentException(ValidationConstant.ERROR_IS_DUPLICATED);
         }
     }
 }
