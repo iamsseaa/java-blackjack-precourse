42f234187ea79e62aa30778e3c6fa7bf0cf18d7f
iamsseaa <homingsea@gmail.com>
2021-12-17 18:19:52 +0900

    feat: 카드를 뽑아 반환

--- src/main/java/domain/card/Deck.java
+++ src/main/java/domain/card/Deck.java
@@ -3,6 +3,7 @@ package domain.card;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class Deck {
     static List<Card> cards = new ArrayList<>();
@@ -10,5 +11,11 @@ public class Deck {
     public static void create() {
         cards.addAll(CardFactory.create());
         Collections.shuffle(cards);
+    }
+
+    public static Card pickCard() {
+        Card card = cards.get(0);
+        cards.remove(0);
+        return card;
     }
 }
