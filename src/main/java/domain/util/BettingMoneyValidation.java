c3932190e9911127744742f0b723cfdd6b8976a5
iamsseaa <homingsea@gmail.com>
2021-12-17 17:09:05 +0900

    feat: 베팅 금액이 숫자인지 확인

--- src/main/java/domain/util/BettingMoneyValidation.java
+++ src/main/java/domain/util/BettingMoneyValidation.java
@@ -1,4 +1,10 @@
 package domain.util;
 
 public class BettingMoneyValidation {
+    public static void isNumber(String bettingMoney) {
+        if (!bettingMoney.matches("-?\\d+")) {
+            throw new IllegalArgumentException(ValidationConstant.ERROR_IS_NOT_NUMBER);
+        }
+    }
+    
 }
