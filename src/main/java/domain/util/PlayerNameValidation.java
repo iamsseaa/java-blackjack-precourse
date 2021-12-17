62b77eda992546728cb2113fd6e660f43f940ff7
iamsseaa <homingsea@gmail.com>
2021-12-17 16:47:32 +0900

    feat: 아무것도 입력되지 않은 예외 처리

--- src/main/java/domain/util/PlayerNameValidation.java
+++ src/main/java/domain/util/PlayerNameValidation.java
@@ -1,4 +1,10 @@
 package domain.util;
 
 public class PlayerNameValidation {
+    public static void isEmpty(String names) {
+        if (names.length() == 0) {
+            throw new IllegalArgumentException(ValidationConstant.ERROR_IS_EMPTY);
+        }
+    }
+
 }
