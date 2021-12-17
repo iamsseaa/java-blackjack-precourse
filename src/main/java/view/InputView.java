d6d0efa422d79f0c1fd297f6d596407749efde76
iamsseaa <homingsea@gmail.com>
2021-12-17 16:18:11 +0900

    feat: 사용자에게 이름 입력 받는 함수

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,22 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+    static Scanner s = new Scanner(System.in);
+
+    public static String inputName() {
+        String names;
+        while (true) {
+            try {
+                System.out.println(Constant.INPUT_NAME);
+                names = s.nextLine();
+                // 예외
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return names;
+    }
 }
