ba4f207495079bf9702446f609e90d88ce44a207
iamsseaa <homingsea@gmail.com>
2021-12-17 16:32:05 +0900

    feat: 첫 두 카드 배분 후 안내 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,16 @@
 package view;
 
+import domain.user.Player;
+
+import java.util.List;
+
 public class OutputView {
+    public static void outputDividedTwoCards(List<Player> players) {
+        System.out.print(Constant.DEALER + Constant.AND);
+        for (Player player : players) {
+            System.out.print(player.getName());
+            System.out.print(Constant.DELIMITER);
+        }
+        System.out.println(Constant.OUTPUT_DIVIDED_CARD);
+    }
 }
