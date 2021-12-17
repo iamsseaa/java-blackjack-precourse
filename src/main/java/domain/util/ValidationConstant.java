5c1531b3ed46702f3f756ae3c01104b9d88b6416
iamsseaa <homingsea@gmail.com>
2021-12-17 16:40:21 +0900

    feat: 예외 처리 관련 안내 문구 상수 클래스 생성

---
+++ src/main/java/domain/util/ValidationConstant.java
@@ -0,0 +1,15 @@
+package domain.util;
+
+public class ValidationConstant {
+
+    public static final String ERROR = "[ERROR] ";
+
+    public static final String ERROR_IS_EMPTY = ERROR + "아무것도 입력되지 않았습니다.";
+    public static final String ERROR_HAS_EMPTY = ERROR + "이름에 공백이 존재하면 안됩니다.";
+    public static final String ERROR_IS_DUPLICATED = ERROR + "이름이 중복됩니다.";
+
+    public static final String ERROR_IS_NOT_NUMBER = ERROR + "베팅 금액은 정수여야합니다.";
+    public static final String ERROR_IS_NOT_POSITIVE = ERROR + "베팅 금액은 1원 이상부터 가능합니다.";
+
+    public static final String ERROR_IS_NOT_Y_N = ERROR + "y 또는 n만 입력 가능합니다.";
+}
